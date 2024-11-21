1d188adeabd444cb579c53550855a38262f23d7c
an-appropriate-username <marxsnow@outlook.com>
2024-11-22 09:09:51 +1300

    User account basics.

--- ViewModels/AccountViewModel.cs
+++ ViewModels/AccountViewModel.cs
@@ -1,19 +1,60 @@
 ﻿using INVApp.Services;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using INVApp.Models;
 
 namespace INVApp.ViewModels
 {
     public class AccountViewModel : BaseViewModel
     {
-        private readonly DatabaseService _databaseService;   
+        private readonly DatabaseService _databaseService;
+
+        private User _currentUser;
+        public User CurrentUser
+        {
+            get => _currentUser;
+            set
+            {
+                if (_currentUser != value)
+                {
+                    _currentUser = value;
+                    OnPropertyChanged(nameof(CurrentUser));
+                }
+            }
+        }
+
+        public ICommand EditProfileCommand { get; }
+        public ICommand LogoutCommand { get; }
 
         public AccountViewModel(DatabaseService databaseService) 
         { 
             _databaseService = databaseService;
-        }  
+
+            CurrentUser = App.CurrentUser;
+
+            // Commands
+            EditProfileCommand = new Command(OnEditProfile);
+            LogoutCommand = new Command(OnLogout);
+        }
+
+        private void OnEditProfile()
+        {
+            // Navigate to Edit Profile Page
+        }
+
+        private void OnLogout()
+        {
+            // Handle Logout
+        }
+
+        public void RefreshCurrentUser()
+        {
+            CurrentUser = App.CurrentUser;
+        }
     }
 }
